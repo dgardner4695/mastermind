6555f60f1e72c61b74242644607306690028d00e
dan-jfisher <dan.johnfisher@gmail.com>
2017-02-02 14:41:55 -0500

    comments

--- mastermind.cpp
+++ mastermind.cpp
@@ -6,21 +6,23 @@
 #include "mastermind.h"
 #include <sstream>
 #include <cstdlib>
-#include "code.h"
 
 
 mastermind::mastermind() : sc(5, 10, false){
+    //default constructor
     numDigits = 5;
     maxDigit = 10;
 
 }
 
 mastermind::mastermind(int num, int max) : sc(num, max, false){
+    //constructor
     numDigits = num;
     maxDigit = max;
 }
 
 code mastermind::getSecretCode() {
+    //return secret code
     return sc;
 }
 
@@ -29,7 +31,7 @@ void mastermind::printSecretCode() {
 
     std::cout << "secret code: ";
     for(int i: codeData){
-        std::cout << codeData[i] << " ";
+        std::cout << codeData[i] << " "; //output each number in the secret code
     }
     std::cout << std::endl;
 }
@@ -43,28 +45,27 @@ int mastermind::getNumDigits() {
 }
 
 code mastermind::humanGuess(){
+    //calls the code constructor using the secret code parameters
     return code(numDigits,maxDigit, true);
 }
 
 response mastermind::getResponse(code sc, code gc){
     int numCorrect = sc.checkCorrect(gc);
     int numIncorrect = sc.checkIncorrect(gc);
-
+    //creates a response
     return response(numCorrect, numIncorrect);
 }
 
 bool mastermind::isSolved(response r){
-    response solved(numDigits, 0);
-    return solved == r;
+    response solved(numDigits, 0); //hypothetical "perfect score" response
+    return solved == r; //overloaded operator checks if they are equal
 }
 
 void mastermind::playGame(){
-    this->printSecretCode();
     bool gameWon = false;
 
-
     while(!gameWon){
-        if(isSolved(getResponse(sc, humanGuess()))){
+        if(isSolved(getResponse(sc, humanGuess()))){ //gets user input -> creates a response -> checks if the response is "solved"
             gameWon = true;
         }
     }
